15e936b07cce0cdbdfbe031adb59e1fa866e4069
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-07-05 01:17:12 +0530

    successfully created Lea dboard component with backend server connected real time changes

--- NodeBackend/index.js
+++ NodeBackend/index.js
@@ -36,6 +36,8 @@ const userSchema = new mongoose.Schema({
   }]
 });
 
+
+
 const commentSchema = new mongoose.Schema({
   postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
